1f7a68d6012d85d430bee606b9743e049487a7be
Gareth Yoder <garethyoder@yahoo.com>
2021-10-19 13:34:15 -0400

    Spring Native conversion

--- software/FunctionOne/src/main/java/helloworld/App.java
+++ software/FunctionOne/src/main/java/helloworld/App.java
@@ -1,55 +1,30 @@
 package helloworld;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import software.amazon.lambda.powertools.metrics.Metrics;
-import software.amazon.lambda.powertools.tracing.Tracing;
+import java.util.function.Function;
 
-import static software.amazon.lambda.powertools.tracing.CaptureMode.DISABLED;
+@SpringBootConfiguration
+public class App implements ApplicationContextInitializer<GenericApplicationContext> {
 
-/**
- * Handler for requests to Lambda function.
- */
-public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-
-    @Tracing(captureMode = DISABLED)
-    @Metrics(captureColdStart = true)
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
-
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
-        }
+    public static void main(String[] args) {
+        FunctionalSpringApplication.run(App.class, args);
     }
 
-    @Tracing(namespace = "getPageContents")
-    private String getPageContents(String address) throws IOException {
-        URL url = new URL(address);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
+    public Function<String, String> uppercase() {
+        return value -> value.toUpperCase();
     }
+
+    @Override
+    public void initialize(GenericApplicationContext context) {
+        context.registerBean("uppercase", FunctionRegistration.class,
+                () -> new FunctionRegistration<>(uppercase())
+                        .type(FunctionType.from(String.class).to(String.class)));
+    }
+
 }
