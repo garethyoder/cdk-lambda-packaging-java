fd528e5f497b1617e29e07113fc27f1930a7c696
Gareth Yoder <garethyoder@yahoo.com>
2021-10-20 11:14:01 -0400

    Update function two to be non-functional

--- software/FunctionTwo/src/main/java/helloworld/App.java
+++ software/FunctionTwo/src/main/java/helloworld/App.java
@@ -1,30 +1,21 @@
 package helloworld;
 
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.cloud.function.context.FunctionRegistration;
-import org.springframework.cloud.function.context.FunctionType;
-import org.springframework.cloud.function.context.FunctionalSpringApplication;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.function.Function;
 
-@SpringBootConfiguration
-public class App implements ApplicationContextInitializer<GenericApplicationContext> {
+@SpringBootApplication
+public class App {
 
     public static void main(String[] args) {
-        FunctionalSpringApplication.run(App.class, args);
+        SpringApplication.run(App.class, args);
     }
 
+    @Bean
     public Function<String, String> lowercase() {
-        return value -> value.toLowerCase();
-    }
-
-    @Override
-    public void initialize(GenericApplicationContext context) {
-        context.registerBean("lowercase", FunctionRegistration.class,
-                () -> new FunctionRegistration<>(lowercase())
-                        .type(FunctionType.from(String.class).to(String.class)));
+        return String::toLowerCase;
     }
 
 }
